20988fee61ecae882a1c80a62cae739976e89aac
Anton Astashov <anton@localhost.(none)>
2009-05-07 09:45:35 +0800

    Improved mechanism of getting messages from debugger (issue #4)

--- vim/bin/ruby_debugger.rb
+++ vim/bin/ruby_debugger.rb
@@ -7,13 +7,24 @@ def create_directory(file)
   dir
 end
 
+ARGV[0] ||= 39767
+ARGV[1] ||= 39768
+ARGV[2] ||= "gvim"
+ARGV[3] ||= "GVIM"
+ARGV[4] ||= "/home/anton/.vim/tmp/ruby_debugger"
+
 def read_socket(response, debugger)
   output = ""
   if response && response[0] && response[0][0]
     output = response[0][0].recv(10000)
-    another_response = select([debugger], nil, nil, 0.01)
-    if another_response && another_response[0] && another_response[0][0]
-      output += read_socket(another_response, debugger)
+    match_opening_tag = output.match(/^<([^\/]+)>/)
+    if match_opening_tag
+      # Check that closing tag is presented. If this is false, there should be next message, wait for it.
+      match_closing_tag = output.match(/<\/#{match_opening_tag[1]}>/)
+      unless match_closing_tag
+        another_response = select([debugger], nil, nil)
+        output += read_socket(another_response, debugger)
+      end
     end
   end
   output
@@ -26,17 +37,15 @@ create_directory(ARGV[4])
 t1 = Thread.new do
   while(session = server.accept)
     input = session.gets
-    break if input.chop == 'stop'
     debugger.puts(input)
   end
 end
 t2 = Thread.new do
   loop do 
     response = select([debugger], nil, nil)
-    # Recursively read socket with interval 0.01 seconds. If there will no message in next 0.01 seconds, stop read.
-    # This is need for reading whole message, because sometimes select/recv reads not all available data
+    puts "Received"
     output = read_socket(response, debugger)
-    output.gsub!(/&quot;/, "\"")
+    puts "Handled"
     File.open(ARGV[4], 'w') { |f| f.puts(output) }
     command = ":call RubyDebugger.receive_command()"
     system("#{ARGV[2]} --servername #{ARGV[3]} -u NONE -U NONE --remote-send \"<C-\\\\><C-N>#{command}<CR>\"");
